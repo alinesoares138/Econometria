595ec6918367c7248a9e8cd47eb87c62ae0a60ec
Aline Soares <71914264+alinesoares138@users.noreply.github.com>
2025-03-14 16:43:26 -0300

    Add files via upload

---
+++ 1_arquivo.R
@@ -0,0 +1,16 @@
+df <- data.frame(
+  y <- c(360, 440, 550, 630, 700),
+  x <- c(100,200,300,400,500)
+)
+modelo <- lm(y ~ x, data = df)
+summary(modelo)
+
+
+summary(modelo)$r.squared
+Ou, manualmente:
+  y_pred <- predict(modelo)
+SQT <- sum((df$y - mean(df$y))^2)  # Variação total de Y
+SQR <- sum((df$y - y_pred)^2)  # Erro do modelo
+
+R2 <- 1 - (SQR / SQT)  
+R2
